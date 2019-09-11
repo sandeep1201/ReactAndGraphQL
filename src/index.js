9f237787bc6fae5a313338163de3a3bce2e37708
Sandeep Reddy Alalla <sra29470@gmail.com>
2019-09-11 14:58:45 -0500

    Nav

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "gestalt/dist/gestalt.css";
 import "./index.css";
 import App from "./App";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Checkout from "./components/Checkout";
+import Navbar from "./components/Navbar";
 import * as serviceWorker from "./serviceWorker";
 
 const Root = () => (
   <Router>
-    <Switch>
-      <Route component={App} exact path="/" />
-      <Route component={Signin} exact path="/signin" />
-      <Route component={Signup} exact path="/signup" />
-      <Route component={Checkout} exact path="/checkout" />
-    </Switch>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route component={App} exact path="/" />
+        <Route component={Signin} exact path="/signin" />
+        <Route component={Signup} exact path="/signup" />
+        <Route component={Checkout} exact path="/checkout" />
+      </Switch>
+    </React.Fragment>
   </Router>
 );
 
